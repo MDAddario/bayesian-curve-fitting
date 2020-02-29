a2651816584f1dc50694f2a4065dc3e65bf34c78
MDAddario <michael.lindner.daddario@gmail.com>
2020-02-29 18:04:19 -0500

    Transfer debug code to separate file in preparation for baeysian curve fitting

--- bayesian-curve-fitting/main.py
+++ bayesian-curve-fitting/main.py
@@ -97,20 +97,13 @@ if __name__ == '__main__':
 	npoints = 10
 
 	args_true = [3, -2, 5]
-	args_fit = [3.2, -2, 4.8]
 	y_error = 1
 
 	x_values, y_values = generate_random_data(quadratic, args_true, y_error, start, end, npoints)
+	
+	# Bayesian curve fit the data
+	args_fit = bayesian_curve_fit()
+	
+	# Visualize the results
 	visualize_fit_data(x_values, y_values, y_error, quadratic, args_fit, args_true)
 
-	# Generate random data set to exponential spread
-	start = 0
-	end = 5
-	npoints = 10
-
-	args_true = 6
-	args_fit = 5.6
-	y_error = 1
-
-	x_values, y_values = generate_random_data(exponential_decay, args_true, y_error, start, end, npoints)
-	visualize_fit_data(x_values, y_values, y_error, exponential_decay, args_fit, args_true)
